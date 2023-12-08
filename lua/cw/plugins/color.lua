a99d3d1b6309090b7b90f1bb453a45ed62d671d9
heychris <senorboo@gmail.com>
2023-12-07 20:44:16 -0600

    feat: disable italics

--- lua/cw/plugins/color.lua
+++ lua/cw/plugins/color.lua
@@ -5,6 +5,7 @@ return {
     require('rose-pine').setup {
       disable_background = true,
       disable_float_background = true,
+      disable_italics = true,
     }
 
     vim.cmd.colorscheme 'rose-pine'
