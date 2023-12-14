f95a2d6155339d9c6f954cf7fcace1309ec168bd
senorboo@gmail.com <senorboo@gmail.com>
2023-12-14 13:41:40 -0600

    feat: add borders to lsp

--- lua/cw/plugins/lsp.lua
+++ lua/cw/plugins/lsp.lua
@@ -145,5 +145,10 @@ return {
         }
       end,
     }
+
+    -- Add borders to LSP windows
+    require('lspconfig.ui.windows').default_options.border = 'single'
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' })
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'single' })
   end,
 }
