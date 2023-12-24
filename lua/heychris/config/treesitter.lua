f943e85bc8a0a465b63fe232c4f1b17cd1ca113c
heychris <senorboo@gmail.com>
2023-12-23 23:20:24 -0600

    refactor: treesitter

---
+++ lua/heychris/config/treesitter.lua
@@ -0,0 +1,89 @@
+local ts_configs = require 'nvim-treesitter.configs'
+
+---@diagnostic disable-next-line: missing-fields
+ts_configs.setup {
+  ensure_installed = {
+    'bash',
+    'query',
+    'html',
+    'javascript',
+    'typescript',
+    'tsx',
+    'json',
+    'markdown',
+    'vim',
+    'vimdoc',
+    'lua',
+    'toml',
+  },
+
+  highlight = {
+    enable = true,
+  },
+
+  indent = {
+    enable = true,
+  },
+
+  additional_vim_regex_highlighting = false,
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<C-space>',
+      node_incremental = '<C-space>',
+      scope_incremental = false,
+      node_decremental = '<bs>',
+    },
+  },
+
+  context_commentstring = {
+    enable = true,
+
+    -- With Comment.nvim, we don't need to run this on the autocmd.
+    -- Only run it in pre-hook
+    enable_autocmd = false,
+
+    config = {
+      lua = '-- %s',
+    },
+  },
+
+  textobjects = {
+    move = {
+      enable = true,
+      goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer' },
+      goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer' },
+      goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer' },
+      goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer' },
+    },
+  },
+
+  playground = {
+    enable = true,
+    updatetime = 25,
+    persist_queries = true,
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+
+      -- This shows stuff like literal strings, commas, etc.
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    },
+  },
+
+  lsp_interop = {
+    enable = true,
+    border = 'solid',
+    peek_definition_code = {
+      ['<Leader>K'] = '@function.outer',
+    },
+  },
+}
