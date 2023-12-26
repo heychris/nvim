f267b9bef1a9971d545ddcf2c74e833759f2cbb7
heychris <senorboo@gmail.com>
2023-12-25 22:07:13 -0600

    feat: update lualine styles

--- lua/heychris/plugins/lualine.lua
+++ lua/heychris/plugins/lualine.lua
@@ -1,5 +1,32 @@
--- Hard code this for now
-local subtle = '#908caa'
+local palette = require 'rose-pine.palette'
+
+local theme = {
+  normal = {
+    a = { fg = palette.rose, bg = palette.overlay },
+    b = { fg = palette.muted, bg = palette.none },
+
+    x = { fg = palette.muted, bg = palette.none },
+  },
+
+  insert = {
+    a = { fg = palette.foam, bg = palette.overlay },
+  },
+
+  visual = {
+    a = { fg = palette.iris, bg = palette.overlay },
+  },
+
+  command = {
+    a = { fg = palette.love, bg = palette.overlay },
+  },
+
+  inactive = {
+    a = { fg = palette.muted, bg = palette.none },
+    b = { fg = palette.muted, bg = palette.none },
+
+    x = { fg = palette.muted, bg = palette.none },
+  },
+}
 
 return {
   'nvim-lualine/lualine.nvim',
@@ -8,16 +35,11 @@ return {
     require('lualine').setup {
       icons_enabled = false,
       sections = {
-        lualine_a = {},
-        lualine_b = { 'mode' },
-        lualine_c = {
-          {
-            'branch',
-            color = { fg = subtle },
-          },
+        lualine_a = { 'mode' },
+        lualine_b = {
+          { 'branch' },
           {
             'filename',
-            color = { fg = subtle },
             file_status = false, -- Displays file status (readonly status, modified status)
             newfile_status = false, -- Display new file status (new file means no write after created)
 
@@ -26,52 +48,54 @@ return {
             -- 2: Absolute path
             -- 3: Absolute path, with tilde as the home directory
             -- 4: Filename and parent dir, with tilde as the home directory
-            path = 1,
+            path = 4,
           },
           {
             'diagnostics',
             sources = { 'nvim_diagnostic' },
             -- Displays diagnostics for the defined severity types
             sections = { 'error', 'warn' },
-            colored = true, -- Displays diagnostics status in color if set to true.
+            colored = false, -- Displays diagnostics status in color if set to true.
             update_in_insert = false, -- Update diagnostics in insert mode.
             always_visible = false, -- Show diagnostics even if there are none.
           },
         },
+        lualine_c = {},
         lualine_x = {
           {
-            'location',
-            color = { fg = subtle },
-          },
-          {
             'filetype',
-            color = { fg = subtle },
           },
           {
             -- Lsp server name .
             function()
               local msg = 'No Active Lsp'
-              local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-              local clients = vim.lsp.get_active_clients()
+              local bufnr = vim.api.nvim_get_current_buf()
+              local clients = vim.lsp.get_clients { bufnr = bufnr }
+
               if next(clients) == nil then
                 return msg
               end
+
+              local client_names = {}
+
               for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                  return client.name
-                end
+                client_names[#client_names + 1] = client.name
               end
-              return msg
+
+              return string.format('[%s]', table.concat(client_names, ', '))
             end,
-            icon = 'LSP:',
-            color = { fg = subtle },
+            icon = false,
           },
+          -- {
+          --   'location',
+          --   color = { fg = subtle },
+          -- },
         },
         lualine_y = {},
         lualine_z = {},
       },
       options = {
+        theme = theme,
         section_separators = '',
         component_separators = '',
       },
