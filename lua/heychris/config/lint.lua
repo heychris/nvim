cbcbd54a4530353dad7affd172718843b4e2a837
senorboo@gmail.com <senorboo@gmail.com>
2024-03-05 16:55:34 -0600

    fix: remove sh from lint options

--- lua/heychris/config/lint.lua
+++ lua/heychris/config/lint.lua
@@ -2,7 +2,6 @@ local lint = require 'lint'
 
 lint.linters_by_ft = {
   lua = { 'luacheck' },
-  sh = { 'shellcheck' },
   markdown = { 'markdownlint' },
   yaml = { 'yamllint' },
   json = { 'jsonlint' },
