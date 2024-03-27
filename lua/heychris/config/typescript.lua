976c4905a95a8b3fcffca7601caa6f08dc8d3034
senorboo@gmail.com <senorboo@gmail.com>
2024-03-27 11:16:11 -0500

    feat: vtsls typescript

--- lua/heychris/config/typescript.lua
+++ lua/heychris/config/typescript.lua
@@ -1,67 +1,25 @@
-local baseDefinitionHandler = vim.lsp.handlers['textDocument/definition']
-
-local function filter(arr, fn)
-  if type(arr) ~= 'table' then
-    return arr
-  end
-
-  local filtered = {}
-  for k, v in pairs(arr) do
-    if fn(v, k, arr) then
-      table.insert(filtered, v)
-    end
-  end
-
-  return filtered
-end
-
-local function filterReactDTS(value)
-  -- Depending on typescript version either uri or targetUri is returned
-  if value.uri then
-    return string.match(value.uri, '%.d.ts') == nil
-  elseif value.targetUri then
-    return string.match(value.targetUri, '%.d.ts') == nil
-  end
-end
-
-local handlers = {
-  ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-    silent = true,
-    border = 'single',
-  }),
-
-  ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = 'single',
-  }),
-
-  ['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
-  }),
-
-  ['textDocument/definition'] = function(err, result, method, ...)
-    if vim.tbl_islist(result) and #result > 1 then
-      local filtered_result = filter(result, filterReactDTS)
-      return baseDefinitionHandler(err, filtered_result, method, ...)
-    end
-
-    baseDefinitionHandler(err, result, method, ...)
-  end,
-}
-
-require('typescript-tools').setup {
-  handlers = handlers,
+require('lspconfig.configs').vtsls = require('vtsls').lspconfig
+require('vtsls').config {
   settings = {
-    publish_diagnostic_on = 'change',
-    expose_as_code_action = 'all',
-    separate_diagnostic_server = true,
-    tsserver_max_memory = 'auto',
-    tsserver_file_preferences = {
-      includeInlayParameterNameHints = 'all',
-      includeCompletionsForModuleExports = true,
-      quotePreference = 'auto',
-    },
-    jsx_close_tag = {
-      enable = true,
+    typescript = {
+      inlayHints = {
+        parameterNames = { enabled = 'literals' },
+        parameterTypes = { enabled = true },
+        variableTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        enumMemberValues = { enabled = true },
+      },
     },
   },
 }
+
+vim.lsp.commands['editor.action.showReferences'] = function(command, ctx)
+  local locations = command.arguments[3]
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
+  if locations and #locations > 0 then
+    local items = vim.lsp.util.locations_to_items(locations, client.offset_encoding)
+    vim.fn.setloclist(0, {}, ' ', { title = 'References', items = items, context = ctx })
+    vim.api.nvim_command 'lopen'
+  end
+end
