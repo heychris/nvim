f4f2eb1ff4f07f5d451dac57d47a000cc492000e
heychris <senorboo@gmail.com>
2025-03-28 22:24:15 -0500

    feat: adjust lualine

--- lua/heychris/config/lualine.lua
+++ lua/heychris/config/lualine.lua
@@ -1,59 +1,33 @@
--- local colors = require 'vesper.colors'
---
--- local theme = {
---   normal = {
---     a = { fg = colors.fgCommand, bg = colors.bgDark },
---     b = { fg = colors.primary, bg = colors.bg },
---
---     x = { fg = colors.primary, bg = colors.bg },
---   },
---   insert = {
---     a = { fg = colors.yellowDark, bg = colors.bgDark },
---   },
---   visual = {
---     a = { fg = colors.green, bg = colors.bgOption },
---   },
---   command = {
---     a = { fg = colors.purple, bg = colors.bgOption },
---   },
---   inactive = {
---     a = { fg = colors.border, bg = colors.bg },
---     b = { fg = colors.border, bg = colors.bg },
---
---     x = { fg = colors.border, bg = colors.bg },
---   },
--- }
-
 local p = require("rose-pine.palette")
 
 local theme = {
   normal = {
-    a = { bg = p.rose, fg = p.base, gui = "bold" },
+    a = { bg = p.none, fg = p.subtle },
     b = { bg = p.none, fg = p.muted },
     c = { bg = p.none, fg = p.muted },
   },
   insert = {
-    a = { bg = p.foam, fg = p.base, gui = "bold" },
+    a = { bg = p.none, fg = p.foam },
     b = { bg = p.none, fg = p.muted },
     c = { bg = p.none, fg = p.muted },
   },
   visual = {
-    a = { bg = p.iris, fg = p.base, gui = "bold" },
+    a = { bg = p.none, fg = p.iris },
     b = { bg = p.none, fg = p.muted },
     c = { bg = p.none, fg = p.muted },
   },
   replace = {
-    a = { bg = p.pine, fg = p.base, gui = "bold" },
+    a = { bg = p.none, fg = p.pine },
     b = { bg = p.none, fg = p.muted },
     c = { bg = p.none, fg = p.muted },
   },
   command = {
-    a = { bg = p.love, fg = p.base, gui = "bold" },
+    a = { bg = p.none, fg = p.love },
     b = { bg = p.none, fg = p.muted },
     c = { bg = p.none, fg = p.muted },
   },
   inactive = {
-    a = { bg = p.base, fg = p.muted, gui = "bold" },
+    a = { bg = p.none, fg = p.base },
     b = { bg = p.none, fg = p.muted },
     c = { bg = p.none, fg = p.muted },
   },
@@ -67,11 +41,12 @@ require("lualine").setup({
   sections = {
     lualine_a = { "mode" },
     lualine_b = {
-      { "branch" },
 
       {
+        color = { fg = p.text },
+
         "filename",
-        file_status = false, -- Displays file status (readonly status, modified status)
+        file_status = true, -- Displays file status (readonly status, modified status)
         newfile_status = false, -- Display new file status (new file means no write after created)
 
         -- 0: Just the filename
@@ -87,7 +62,7 @@ require("lualine").setup({
         sources = { "nvim_diagnostic" },
         -- Displays diagnostics for the defined severity types
         sections = { "error", "warn" },
-        colored = false, -- Displays diagnostics status in color if set to true.
+        colored = true, -- Displays diagnostics status in color if set to true.
         update_in_insert = false, -- Update diagnostics in insert mode.
         always_visible = false, -- Show diagnostics even if there are none.
       },
@@ -95,49 +70,28 @@ require("lualine").setup({
     lualine_c = {},
     lualine_x = {
       {
-        function()
-          local linters = require("lint").get_running()
-
-          if #linters == 0 then
-            return ""
-          end
-          return table.concat(linters, ", ")
-        end,
+        color = { fg = p.subtle },
+        "branch",
       },
 
       {
-        "filetype",
-        colored = false,
-      },
-
-      {
-        -- Lsp server name .
         function()
           local msg = "No Active Lsp"
-          local bufnr = vim.api.nvim_get_current_buf()
-          local clients = vim.lsp.get_clients({ bufnr = bufnr })
-
+          local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
+          local clients = vim.lsp.get_clients()
           if next(clients) == nil then
             return msg
           end
-
-          local client_names = {}
           for _, client in ipairs(clients) do
-            client_names[#client_names + 1] = client.name
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+              return string.format("[%s]", client.name)
+            end
           end
-
-          if #client_names > 2 then
-            return string.format("[%s, %s, and %d more]", client_names[1], client_names[2], #client_names - 2)
-          end
-
-          return string.format("[%s]", table.concat(client_names, ", "))
+          return msg
         end,
         icon = false,
       },
-      -- {
-      --   'location',
-      --   color = { fg = subtle },
-      -- },
     },
     lualine_y = {},
     lualine_z = {},
--- lua/heychris/plugins/color.lua
+++ lua/heychris/plugins/color.lua
@@ -109,34 +109,7 @@ return {
           comment = mix("#8b8b8b", "#000000", math.abs(0.94)),
         },
       })
-      -- vim.cmd.colorscheme 'vesper'
+      --vim.cmd.colorscheme("vesper")
     end,
   },
-  -- {
-  --   'RRethy/base16-nvim',
-  --   lazy = false,
-  --   config = function()
-  --     --vim.cmd 'colorscheme base16-black-metal-bathory'
-  --     require('base16-colorscheme').setup {
-  --       base00 = '#000000',
-  --       base01 = '#121212',
-  --       base02 = '#222222',
-  --       --base03 = '#333333',
-  --       base03 = '#999999',
-  --       base04 = '#999999',
-  --       --base05 = '#c1c1c1',
-  --       base05 = '#eeeeee',
-  --       base06 = '#999999',
-  --       base07 = '#c1c1c1',
-  --       base08 = '#5f8787',
-  --       base09 = '#aaaaaa',
-  --       base0A = '#e78a53',
-  --       base0B = '#fbcb97',
-  --       base0C = '#aaaaaa',
-  --       base0D = '#888888',
-  --       base0E = '#999999',
-  --       base0F = '#444444',
-  --     }
-  --   end,
-  -- },
 }
